706b8ee0bfdaa9be31a4e8404d7614a20420c9ea
Gregory Poole <gbpoole@gmail.com>
2017-12-01 23:39:49 +1100

    Move all files from original gbpSID repo that we want to seed this repo with to keep.

--- src/file_io/SID_reset_fp_buffer.c
+++
@@ -1,8 +0,0 @@
-#include <gbpSID.h>
-
-void SID_reset_fp_buffer(SID_fp_buffer **fp_buffer) {
-    (*fp_buffer)->n_bytes_unread             = (*fp_buffer)->n_bytes_to_read;
-    (*fp_buffer)->n_bytes_buffer_unprocessed = 0; // This way we will perform a read right away
-    (*fp_buffer)->n_bytes_buffer_processed   = 0;
-    (*fp_buffer)->n_bytes_buffer             = 0;
-}
--- src/mpi/SID_Bcast.c
+++
@@ -1,9 +0,0 @@
-#include <gbpSID.h>
-
-#define MAX_SEND_LOCAL 10 * SID_SIZE_OF_MEGABYTE
-
-void SID_Bcast(void *buffer, int count, SID_Datatype datatype, int source_rank, SID_Comm *comm) {
-#if USE_MPI
-    MPI_Bcast(buffer, count, datatype, source_rank, (MPI_Comm)(comm->comm));
-#endif
-}
