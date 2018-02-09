1b8646d00194eb281482a4ac7c9e203588c0487a
Gregory Poole <gbpoole@gmail.com>
2018-02-09 13:21:03 +1100

    Changed hello_world.c to a template.  Some cmake debugging.

--- python/gbpBuild/gbpBuild/data/support/cmake/custom_targets.cmake
+++ python/gbpBuild/gbpBuild/data/support/cmake/custom_targets.cmake
@@ -1,52 +1,90 @@
 # This file contains all the macros that are used
 #   by cmake to specify custom targets
 
-# Create a master list of all source and header files
-set(ALL_FILES_PROJECT "" )
-list(APPEND ALL_FILES_PROJECT ${INC_FILES_PROJECT} )
-list(APPEND ALL_FILES_PROJECT ${SRC_FILES_PROJECT} )
-
 # Add clang-related targets
 macro(add_custom_clang)
+    # Create a master list of all source and header files
+    # This needs to be done here (rather than when this file
+    # is included) because we need to have processed the
+    # project tree first, to have a list of files in place.
+    set(ALL_FILES_PROJECT "" )
+    list(APPEND ALL_FILES_PROJECT ${INC_FILES_PROJECT} )
+    list(APPEND ALL_FILES_PROJECT ${SRC_FILES_PROJECT} )
+
     # Clang-format
-    add_custom_target(
-        clang-format-suggest
-        COMMAND clang-format
-        -style=file
-        ${ALL_FILES_PROJECT}
-    )
-    message(STATUS "   -> clang-format-suggest ; write clang-format suggestions to stdout")
+    if(ALL_FILES_PROJECT)
+        add_custom_target(
+            clang-format-check
+            COMMAND clang-format
+            -style=file
+            ${ALL_FILES_PROJECT}
+        )
+        message(STATUS "   -> clang-format-check ; write clang-format suggestions to stdout")
 
-    add_custom_target(
-        clang-format-fix
-        COMMAND clang-format
-        -style=file
-        -i
-        ${ALL_FILES_PROJECT}
-    )
-    message(STATUS "   -> clang-format-fix     ; apply the project style standards using clang-format")
+        add_custom_target(
+            clang-format-fix
+            COMMAND clang-format
+            -style=file
+            -i
+            ${ALL_FILES_PROJECT}
+        )
+        message(STATUS "   -> clang-format-fix   ; apply the project style standards using clang-format")
 
-    # Clang-tidy
-    add_custom_target(
-        clang-tidy-suggest
-        COMMAND clang-tidy
-        -p ${CMAKE_CURRENT_BINARY_DIR}
-        ${SRC_FILES_PROJECT}
-    )
-    message(STATUS "   -> clang-tidy-suggest   ; write clang-tidy suggestions to stdout")
-    add_custom_target(
-        clang-tidy-fix
-        COMMAND clang-tidy
-        -p ${CMAKE_CURRENT_BINARY_DIR}
-        -fix
-        ${SRC_FILES_PROJECT}
-    )
-    message(STATUS "   -> clang-tidy-fix       ; apply all clang-tidy suggestions in-place ... BE CAREFUL WITH THIS!")
+        # Clang-tidy
+        add_custom_target(
+            clang-tidy-check
+            COMMAND clang-tidy
+            -p ${CMAKE_CURRENT_BINARY_DIR}
+            ${SRC_FILES_PROJECT}
+        )
+        message(STATUS "   -> clang-tidy-check   ; write clang-tidy suggestions to stdout")
+        add_custom_target(
+            clang-tidy-fix
+            COMMAND clang-tidy
+            -p ${CMAKE_CURRENT_BINARY_DIR}
+            -fix
+            ${SRC_FILES_PROJECT}
+        )
+        message(STATUS "   -> clang-tidy-fix     ; apply all clang-tidy suggestions in-place ... BE CAREFUL WITH THIS!")
+    else()
+        add_custom_target(
+            clang-format-check
+            COMMAND echo No files to run clang-format on.
+        )
+        message(STATUS "   -> clang-format-check ; *** NO FILES TO PROCESS")
+
+        add_custom_target(
+            clang-format-fix
+            COMMAND echo No files to run clang-format on.
+        )
+        message(STATUS "   -> clang-format-fix   ; *** NO FILES TO PROCESS")
+
+        # Clang-tidy
+        add_custom_target(
+            clang-tidy-check
+            COMMAND echo No files to run clang-tidy on.
+        )
+        message(STATUS "   -> clang-tidy-check   ; *** NO FILES TO PROCESS")
+        add_custom_target(
+            clang-tidy-fix
+            COMMAND echo No files to run clang-tidy on.
+        )
+        message(STATUS "   -> clang-tidy-fix     ; *** NO FILES TO PROCESS")
+    endif()
 endmacro()
 
 # Add the targets used to generate documentation
 macro(add_custom_docs)
     if(GBP_DOCS_BUILD_FOUND)
+        # Create a master list of all source and header files
+        # This needs to be done here (rather than when this file
+        # is included) because we need to have processed the
+        # project tree first, to have a list of files in place.
+        # Note that SRC_FILES_PROJECT includes executable files.
+        set(ALL_FILES_PROJECT "" )
+        list(APPEND ALL_FILES_PROJECT ${INC_FILES_PROJECT} )
+        list(APPEND ALL_FILES_PROJECT ${SRC_FILES_PROJECT} )
+
         # Tell Doxygen to only configure XML.  Sphynx will do the rest.
         set(DOXYGEN_GENERATE_XML   YES)
         set(DOXYGEN_GENERATE_LATEX  NO)
@@ -72,7 +110,7 @@ macro(add_custom_docs)
         add_custom_target(
             docs-api
             COMMENT "Generate API documentation files")
-        message(STATUS "   -> docs-api             ; generate API documentation files")
+        message(STATUS "   -> docs-api           ; generate API documentation files")
 
         # Set build dependancies for documentation
         add_dependencies(docs-breathe docs-doxygen )
