88d7b913c6a4a43ef12d476ad68b32a8c3ee6af3
Gregory Poole <gbpoole@gmail.com>
2018-07-31 23:26:11 +1000

    Fixed a minor PY2/3 compatibility bug.

--- python/gbpBuild/gbpBuild/_internal/__init__.py
+++ python/gbpBuild/gbpBuild/_internal/__init__.py
@@ -1,28 +1,33 @@
 """This module is generally only intended for use by developers of this
 package.  It provides three principle submodules for:
 
-1) _internal.package: for examining meta data relating to this python package
-2) _internal.project: for examining meta data relating to the project this package belongs to
-3) _internal.log: for generating course logging information for the user
+1) _internal.package: for examining meta data relating to this python
+package 2) _internal.project: for examining meta data relating to the
+project this package belongs to 3) _internal.log: for generating course
+logging information for the user
 """
+
+import sys
 
 PY2 = sys.version_info[0] == 2
 if not PY2:
     string_types = (str,)
 else:
-    string_types = (str,unicode)
+    string_types = (str, unicode)
+
 
 def is_nonstring_iterable(object_in):
     """Determine if an object is a non-string iterable.
- 
+
     :param object: An object of any type.
     :return: Boolean.  True if object is a non-string iterable.
     """
     return hasattr(object_in, '__iter__') and not isinstance(object_in, string_types)
 
+
 def ascii_encode_value(value):
     """Encode an object as ascii, if possible.
- 
+
     :param object: An object of any type.
     :return: An ascii encoding of the object.  The object if that encoding is not defined.
     """
@@ -35,9 +40,10 @@ def ascii_encode_value(value):
         result = value
     return result
 
+
 def ascii_encode_dict(data):
     """Encode the keys and values of a dictionary as ascii.
- 
+
     :param object: A dictionary.
     :return: An ascii encoding of the dictionary.
     """
@@ -51,4 +57,4 @@ def ascii_encode_dict(data):
     # Convert values
     values_ascii = [ascii_encode_value(value) for value in values]
     # Return dictionary as a result
-    return dict(zip(keys_ascii,values_ascii))
+    return dict(zip(keys_ascii, values_ascii))
